510954b367ede486c24f0ddb32325595aa1f6f12
Lloyd Boone <lloydboone@Lloyds-MacBook-Pro.local>
2016-08-18 09:31:02 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="js/phaser/phaser.min.js"></script>
+        <script src="js/state1"></script>
+        
+    </head>
+    
+    <body>
+        
+        <script src="js/main.js"></script>
+    </body>
+</html>
